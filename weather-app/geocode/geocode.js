ef364497e743e0c3af5071eb5187d8101d68e7c5
Ananthbhat <ananthnbhat2@gmail.com>
2017-10-03 23:18:13 +0530

    nodejs apps

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -9,8 +9,9 @@ var geocodeAddress = (address, callback) => {
     }, (error, response, body) => {
         if (error) {
             callback('Unable to connect to Google servers.');
-        } else if (body.status === 'ZERO_RESULTS') {
+        } else if (body.status === "ZERO_RESULTS") {
             callback('Unable to find that address.');
+
         } else if (body.status === 'OK') {
             callback(undefined, {
                 address: body.results[0].formatted_address,
