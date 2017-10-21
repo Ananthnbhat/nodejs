110aac3b36bfb9258a03953e460b0f7add637a37
Ananthbhat <ananthnbhat2@gmail.com>
2017-10-21 23:49:02 +0530

    commit 3.0

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,8 @@ const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
 
+const port=process.env.PORT || 3000;
+
 var app=express();
 
 hbs.registerPartials(__dirname +'/views/partials');
@@ -21,9 +23,9 @@ app.use((req,res,next)=>{
 
 });
 
-app.use((req,res,next)=>{
-    res.render('maintenance.hbs');
-});
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+// });
 
 
 hbs.registerHelper('getCurrentYear',()=>{
@@ -53,4 +55,6 @@ res.send({
 errorMessage:'Unable to handle request'
 });
 });
-app.listen(3000); 
+app.listen(port,()=>{
+    console.log(`The server is running on ${port}`);
+}); 
