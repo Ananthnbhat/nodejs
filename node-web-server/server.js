0fceed8720c179b9728d001bbc5b4255d64c58c3
Ananthbhat <ananthnbhat2@gmail.com>
2017-10-21 14:57:46 +0530

    node-web-server

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,1 +1,56 @@
 const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname +'/views/partials');
+app.set('view engine','hbs');
+// app.use(express.static(__dirname +'/public'));
+
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log=`${now}:${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{ 
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+
+});
+
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+});
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('scream',(text)=>{
+return text .toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+// res.send('<h1>Hello express</h1>');
+res.render('home.hbs',{
+    pageTitle:'Home page',
+    wm:'Welcome to this website'
+});
+});
+
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+    pageTitle:'About page'
+});
+});
+
+app.get('/bad',(req,res)=>{
+res.send({
+errorMessage:'Unable to handle request'
+});
+});
+app.listen(3000); 
